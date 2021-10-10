ff3905cd6cb3c606a9d7a5166c0f8ccec1a8c43d
Yash Gupta <yash8917gupta@gmail.com>
2021-10-11 00:21:33 +0530

    Initial Commit

--- src/Solution1.java
+++ src/Solution1.java
@@ -16,6 +16,7 @@ In each line of output there should be two columns:
 The first column contains the String and is left justified using exactly 15 characters.
 The second column contains the integer, expressed in exactly 3 digits; if the original input has less than three digits, you must pad your output's leading digits with zeroes.
  */
+
 import java.util.Scanner;
 
 public class Solution1 {
