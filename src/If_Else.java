7b8df7ef92835e1d83dbc15f14fc5567f2dbf1f5
Yash Gupta <yash8917gupta@gmail.com>
2021-10-15 22:25:46 +0530

    Initial Commit

--- src/If_Else.java
+++ src/If_Else.java
@@ -18,7 +18,6 @@ import java.io.*;
 public class If_Else {
 
     private static final Scanner scanner = new Scanner(System.in);
-
     public static void main(String[] args) {
         int N = scanner.nextInt();
         scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
