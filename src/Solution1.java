fe6bf2e08018f4274e723aac66288e28b7627d67
Yash Gupta <yash8917gupta@gmail.com>
2021-10-15 22:23:55 +0530

    Initial Commit

--- src/Solution1.java
+++ src/Solution1.java
@@ -21,7 +21,6 @@ import java.util.Scanner;
 
 public class Solution1 {
 //Start the main class
-
     public static void main(String[] args) {
         Scanner sc=new Scanner(System.in);
         System.out.println("================================");
