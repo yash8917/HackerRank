c8e03064e3297088893321a50c5a03d8e9178916
Yash Gupta <yash8917gupta@gmail.com>
2021-10-15 22:23:17 +0530

    Initial Commit

--- src/Solution.java
+++ src/Solution.java
@@ -36,6 +36,7 @@ public class Solution {
         int i = scan.nextInt();
         double d= scan.nextDouble();
         scan.nextLine();
+
         String s=scan.nextLine();
         System.out.println("String: " + s);
         System.out.println("Double: " + d);
