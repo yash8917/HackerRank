686cb68836ae68b14c7b0581a31f05d8c916c9a0
Yash Gupta <yash8917gupta@gmail.com>
2021-10-15 22:22:37 +0530

    Initial Commit

--- src/Solution.java
+++ src/Solution.java
@@ -30,7 +30,6 @@ Note: We recommend completing Java Stdin and Stdout I before attempting this cha
 import java.util.Scanner;
 
 public class Solution {
-
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         int i = scan.nextInt();
